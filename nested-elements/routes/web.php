b7015240d20340a2738990b93eb9b494bd43d520
JoniMercura <jsm@mercura.dk>
2024-07-29 11:27:08 +0200

    tenant test

--- nested-elements/routes/web.php
+++ nested-elements/routes/web.php
@@ -7,25 +7,29 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\Auth\LoginController;
 
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            return redirect('/admin');
+        }); 
+        
+        Route::get('/create-product', [ProductController::class, 'create']);
+        Route::get('/product/{product}', [ProductController::class, 'show']);
+        Route::get('/test-recursive-relationships', [ProductController::class, 'testRecursiveRelationships']);
+        Route::get('/tree', [ProductController::class, 'tree']);
+        Route::get('/test-additional-methods/{childId}/{parentId}', [ProductController::class, 'testAdditionalMethods']);
+        
+        Route::get('assembly/{assembly}', [AssemblyController::class, 'show']);
+        Route::get('group/init', [GroupController::class, 'init']);
+        
+        
+        // Auth
+        Route::get('auth/google', [LoginController::class, 'redirectToGoogle']);
+        Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
+        
+        Route::get('auth/microsoft', [LoginController::class, 'redirectToMicrosoft']);
+        Route::get('auth/microsoft/callback', [LoginController::class, 'handleMicrosoftCallback']);
+    });
+}
 
-Route::get('/', function () {
-    return redirect('/admin');
-}); 
-
-Route::get('/create-product', [ProductController::class, 'create']);
-Route::get('/product/{product}', [ProductController::class, 'show']);
-Route::get('/test-recursive-relationships', [ProductController::class, 'testRecursiveRelationships']);
-Route::get('/tree', [ProductController::class, 'tree']);
-Route::get('/test-additional-methods/{childId}/{parentId}', [ProductController::class, 'testAdditionalMethods']);
-
-Route::get('assembly/{assembly}', [AssemblyController::class, 'show']);
-Route::get('group/init', [GroupController::class, 'init']);
-
-
-// Auth
-Route::get('auth/google', [LoginController::class, 'redirectToGoogle']);
-Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
-
-Route::get('auth/microsoft', [LoginController::class, 'redirectToMicrosoft']);
-Route::get('auth/microsoft/callback', [LoginController::class, 'handleMicrosoftCallback']);
 
